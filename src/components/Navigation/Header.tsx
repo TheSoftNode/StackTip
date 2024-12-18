acb001e32e3e1c0a81777b225dfec1ed87784c4f
TheSoftNode <thesoftnode@gmail.com>
2024-12-19 00:51:41 +0100

    Add logo

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -1,19 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Bell, Menu, Home, Layout, LogOutIcon, WalletIcon } from 'lucide-react';
 import { useAppContext } from '@/context/AppContext';
-import {
-  authenticate,
-  getUserData,
-  signUserOut,
-  userSession,
-} from '@/lib/auth';
+import
+  {
+    authenticate,
+    getUserData,
+    signUserOut,
+    userSession,
+  } from '@/lib/auth';
 import { UserData } from '@/lib/type';
 import toast from 'react-hot-toast';
 import { authContext } from '@/context/AuthContext';
 import EmailModal from '../Utils/EmailModal';
 import VerifyEmailModal from '../Utils/VerifyEmailModal';
+import logo from '/logo.svg';
 
-export const Header = () => {
+export const Header = () =>
+{
   const [email, setEmail] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [userData, setUserData] = useState<UserData | null>(null);
@@ -23,141 +26,146 @@ export const Header = () => {
   const [isVerifyModalOpen, setIsVerifyModalOpen] = useState(false);
 
   console.log(userData);
-  
-  const {walletAddress, setWalletAddress, currentPage, setCurrentPage, setWalletConnected } = useAppContext();
+
+  const { walletAddress, setWalletAddress, currentPage, setCurrentPage, setWalletConnected } = useAppContext();
   const { dispatch } = useContext(authContext);
 
   useEffect(() =>
+  {
+    const checkAuth = async () =>
     {
-      const checkAuth = async () =>
+      if (userSession.isSignInPending())
       {
-        if (userSession.isSignInPending())
-        {
-          const userData = await userSession.handlePendingSignIn();
-          setIsAuthenticated(true);
-          setUserData(userData);
-          setWalletAddress(userData?.profile?.stxAddress?.testnet ?? null);
-          setWalletConnected(true);
-          await checkUserExists(userData?.profile?.stxAddress?.testnet);
-        } else if (userSession.isUserSignedIn())
-        {
-          const userData = getUserData();
-          setIsAuthenticated(true);
-          setUserData(userData);
-          setWalletAddress(userData?.profile?.stxAddress?.testnet ?? null);
-          setWalletConnected(true);
-          await checkUserExists(userData?.profile?.stxAddress?.testnet);
-        }
-      };
-  
-      checkAuth();
-    }, []);
-
-    const checkUserExists = async (walletAddress: string | null) =>
+        const userData = await userSession.handlePendingSignIn();
+        setIsAuthenticated(true);
+        setUserData(userData);
+        setWalletAddress(userData?.profile?.stxAddress?.testnet ?? null);
+        setWalletConnected(true);
+        await checkUserExists(userData?.profile?.stxAddress?.testnet);
+      } else if (userSession.isUserSignedIn())
       {
-        if (!walletAddress) return;
-    
-        try
-        {
-          const response = await fetch(`https://stx-tip.onrender.com/api/v1/users/exists?wallet=${walletAddress.toLowerCase()}`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-    
-    
-          if (!response.ok)
-          {
-            throw new Error('Failed to check user existence');
-          }
-    
-          const data = await response.json();
-          if (!data.exists)
-          {
-            setIsEmailModalOpen(true);
-          }
-        } catch (error)
-        {
-          console.error('Failed to check user existence:', error);
-        }
-      };
+        const userData = getUserData();
+        setIsAuthenticated(true);
+        setUserData(userData);
+        setWalletAddress(userData?.profile?.stxAddress?.testnet ?? null);
+        setWalletConnected(true);
+        await checkUserExists(userData?.profile?.stxAddress?.testnet);
+      }
+    };
 
-  const handleAuth = () => {
-    if (isAuthenticated) {
+    checkAuth();
+  }, []);
+
+  const checkUserExists = async (walletAddress: string | null) =>
+  {
+    if (!walletAddress) return;
+
+    try
+    {
+      const response = await fetch(`https://stx-tip.onrender.com/api/v1/users/exists?wallet=${walletAddress.toLowerCase()}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+
+      if (!response.ok)
+      {
+        throw new Error('Failed to check user existence');
+      }
+
+      const data = await response.json();
+      if (!data.exists)
+      {
+        setIsEmailModalOpen(true);
+      }
+    } catch (error)
+    {
+      console.error('Failed to check user existence:', error);
+    }
+  };
+
+  const handleAuth = () =>
+  {
+    if (isAuthenticated)
+    {
       signUserOut();
       setWalletAddress(null);
       setIsAuthenticated(false);
-    } else {
+    } else
+    {
       authenticate();
     }
   };
 
-  const toggleDashboard = () => {
+  const toggleDashboard = () =>
+  {
     setCurrentPage(currentPage === 'home' ? 'dashboard' : 'home');
   };
 
-  const formatWalletAddress = (address: string) => {
+  const formatWalletAddress = (address: string) =>
+  {
     if (!address) return '';
     return `${address.slice(0, 6)}...${address.slice(-4)}`;
   };
 
   const handleEmailSubmit = async (e: React.FormEvent<HTMLFormElement>) =>
+  {
+    e.preventDefault();
+    if (!email.trim().toLowerCase() || !walletAddress?.toLowerCase()) return;
+
+    console.log('email:', email);
+    console.log('wallet:', walletAddress);
+
+    try
     {
-      e.preventDefault();
-      if (!email.trim().toLowerCase() || !walletAddress?.toLowerCase()) return;
-  
-      console.log('email:', email);
-      console.log('wallet:', walletAddress);
-  
-      try
+      setIsLoading(true);
+
+      const response = await fetch('https://stx-tip.onrender.com/api/v1/users/connect-wallet', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email.trim().toLowerCase(),
+          wallet: walletAddress
+        }),
+      });
+
+      const result = await response.json();
+
+      if (response.ok)
       {
-        setIsLoading(true);
-  
-        const response = await fetch('https://stx-tip.onrender.com/api/v1/users/connect-wallet', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email: email.trim().toLowerCase(),
-            wallet: walletAddress
-          }),
-        });
-  
-        const result = await response.json();
-  
-        if (response.ok)
-        {
-          dispatch({
-            type: "ACTIVATE_USER",
-            payload: {
-              activationToken: result.verificationToken,
-              activation_Code: result.activationCode
-            }
-          })
-  
-          console.log(result.activationCode)
-  
-          setIsLoading(false);
-          setIsVerifyModalOpen(true);
-        }
-        else
-        {
-          toast.error(result.message);
-          setIsLoading(false);
-        }
-  
-        setIsEmailModalOpen(false);
-      } catch (error: any)
+        dispatch({
+          type: "ACTIVATE_USER",
+          payload: {
+            activationToken: result.verificationToken,
+            activation_Code: result.activationCode
+          }
+        })
+
+        console.log(result.activationCode)
+
+        setIsLoading(false);
+        setIsVerifyModalOpen(true);
+      }
+      else
       {
-        console.error('Failed to save user data:', error);
-        toast.error(error.message);
-      } finally
-      {
+        toast.error(result.message);
         setIsLoading(false);
       }
-    };
+
+      setIsEmailModalOpen(false);
+    } catch (error: any)
+    {
+      console.error('Failed to save user data:', error);
+      toast.error(error.message);
+    } finally
+    {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
@@ -170,9 +178,10 @@ export const Header = () => {
                 <Menu className="h-6 w-6" />
               </button>
             )}
-            
-            <div className="flex items-center space-x-2">
-              <div className="text-xl md:text-2xl font-bold bg-gradient-to-r from-violet-600 to-purple-600 bg-clip-text text-transparent transform hover:scale-105 transition-transform cursor-pointer">
+
+            <div className="flex items-center">
+              {/* <img src={logo} alt="logo" className="h-24 w-16 sm:h-28 sm:w-24 md:h-36 md:w-32" /> */}
+              <div className="text-sm sm:text-xl md:text-2xl font-bold bg-gradient-to-r from-violet-600 to-purple-600 bg-clip-text text-transparent transform hover:scale-105 transition-transform cursor-pointer">
                 TipStack
               </div>
             </div>
@@ -180,11 +189,10 @@ export const Header = () => {
             {isAuthenticated && (
               <button
                 onClick={toggleDashboard}
-                className={`flex items-center space-x-2 px-2 py-1 rounded-lg transition-all duration-200  ${
-                  currentPage === (currentPage === 'home' ? 'dashboard' : 'home')
+                className={`flex items-center space-x-2 px-2 py-1 rounded-lg transition-all duration-200  ${currentPage === (currentPage === 'home' ? 'dashboard' : 'home')
                     ? 'bg-gradient-to-r from-violet-600/10 to-purple-600/10 text-purple-700'
                     : 'hover:bg-gray-100'
-                }`}
+                  }`}
               >
                 {currentPage === 'home' ? (
                   <>
@@ -209,7 +217,7 @@ export const Header = () => {
                   <Bell className="h-6 w-6" />
                   <span className="absolute top-0 right-0 h-2 w-2 bg-red-500 rounded-full"></span>
                 </button>
-                
+
                 <div className="hidden md:flex items-center px-4 py-2 bg-gradient-to-r from-violet-600/10 to-purple-600/10 rounded-lg border border-purple-100">
                   <WalletIcon className="h-4 w-4 text-purple-600 mr-2" />
                   <span className="text-sm font-medium text-purple-700">
